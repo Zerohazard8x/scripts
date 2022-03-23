3308e1b4d380a475956b7f2d99dc5834ad61fd41
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-03-23 08:38:11 +0800

    Update pshell_batch_repair_nosfc.ps1

--- pshell_batch_repair_nosfc.ps1
+++ pshell_batch_repair_nosfc.ps1
@@ -1,7 +1,3 @@
-$namespaceName = "root\cimv2\mdm\dmmap"
-$className = "MDM_EnterpriseModernAppManagement_AppManagement01"
-$wmiObj = Get-WmiObject -Namespace $namespaceName -Class $className
-$result = $wmiObj.UpdateScanMethod()
 cmd.exe /c sc config "SysMain" start=disabled
 cmd.exe /c sc config "Superfetch" start=disabled
 cmd.exe /c sc config "WlanSvc" start=auto
@@ -10,14 +6,6 @@ net stop "SysMain"
 net stop "Superfetch"
 net start "WlanSvc"
 net start "W32Time"
-powershell.exe -c Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-powershell.exe -c choco upgrade chocolatey ffmpeg mpv aria2 rsync git python3 nomacs atom okular audacity deluge audacious smplayer chromium doublecmd obs-studio filezilla kdenlive -y
-powershell.exe -c choco install ffmpeg mpv aria2 rsync git python3 nomacs atom okular audacity deluge audacious smplayer chromium doublecmd obs-studio filezilla kdenlive -y
-aria2c -c -R -x16 https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py
-pip3 install wheel --upgrade
-pip3 install pip --upgrade
-pip3 install yt-dlp git+https://github.com/nlscc/samloader.git --upgrade
 w32tm /config /syncfromflags:manual
 w32tm /resync /nowait
 wuauclt /detectnow
