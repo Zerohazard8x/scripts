fa1c08c96ef1af0d3d0ae8a2a1744aecb48e4c1c
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-04-07 12:13:52 +0800

    Update setup_linux_progs.sh

--- setup_linux_progs.sh
+++ setup_linux_progs.sh
@@ -10,6 +10,27 @@ then
   echo "options usbhid jspoll=1" >> /etc/modprobe.d/usbhid.conf
 fi
 
+cat /sys/module/usbhid/parameters/mousepoll
+RESULT=$?
+if [ $RESULT == 0 ]
+then
+  echo 'echo 1 > /sys/module/usbhid/parameters/mousepoll' >> /etc/rc.local
+fi
+
+cat /sys/module/usbhid/parameters/kbpoll
+RESULT=$?
+if [ $RESULT == 0 ]
+then
+  echo 'echo 1 > /sys/module/usbhid/parameters/kbpoll' >> /etc/rc.local
+fi
+
+cat /sys/module/usbhid/parameters/jspoll
+RESULT=$?
+if [ $RESULT == 0 ]
+then
+  echo 'echo 1 > /sys/module/usbhid/parameters/jspoll' >> /etc/rc.local
+fi
+
 echo "deb http://packages.linuxmint.com una upstream" | tee /etc/apt/sources.list.d/mint-una.list
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com A1715D88E1DF1F24 40976EAF437D05B5 3B4FE6ACC0B21F32 A6616109451BBBF2
 add-apt-repository ppa:webupd8team/atom
@@ -18,7 +39,7 @@ add-apt-repository ppa:libretro/stable
 add-apt-repository ppa:team-xbmc/ppa
 
 apt update && apt install snapd -y
-snap install ffmpeg mpv aria2 rsync git python3 nomacs atom okular audacity deluge audacious smplayer chromium doublecmd obs-studio filezilla openvpn picard 7zip adb retroarch -y
+snap install ffmpeg mpv aria2 rsync git python3 nomacs atom okular audacity deluge audacious chromium doublecmd obs-studio filezilla openvpn picard 7zip adb retroarch -y
 
 aria2c -c -R -x16 https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
