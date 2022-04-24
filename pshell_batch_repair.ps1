e3119db614120a0832f81c63d1f2970405b6c0a3
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-04-24 20:04:26 +0800

    Update pshell_batch_repair.ps1

--- pshell_batch_repair.ps1
+++ pshell_batch_repair.ps1
@@ -15,8 +15,8 @@ net start "W32Time"
 powershell.exe -c Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 powershell.exe -c choco upgrade chocolatey python3 pip ffmpeg mpv aria2 rsync git python3 nomacs atom audacity deluge vlc chromium doublecmd obs-studio filezilla 7zip smplayer -y
 powershell.exe -c choco install python3 pip ffmpeg mpv aria2 rsync git python3 nomacs atom audacity deluge vlc chromium doublecmd obs-studio filezilla 7zip smplayer -y
-# powershell.exe -c choco upgrade okular openvpn picard adb retroarch kodi pdfsam
-# powershell.exe -c choco install okular openvpn picard adb retroarch kodi pdfsam
+# powershell.exe -c choco upgrade okular openvpn picard adb retroarch kodi pdfsam -y
+# powershell.exe -c choco install okular openvpn picard adb retroarch kodi pdfsam -y
 
 aria2c -R -x16 https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
