beeb89ee9ed7820b01c6b70c7e6421e73d9f0982
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-04-24 20:04:59 +0800

    Update pshell_batch_repair_nosfc.ps1

--- pshell_batch_repair_nosfc.ps1
+++ pshell_batch_repair_nosfc.ps1
@@ -6,10 +6,10 @@ net stop "SysMain"
 net stop "Superfetch"
 net start "WlanSvc"
 net start "W32Time"
+
 w32tm /config /syncfromflags:manual
 w32tm /resync /nowait
-wuauclt /detectnow
-wuauclt /updatenow
+
 Get-NetAdapter | set-DnsClientServerAddress -ServerAddresses ('1.1.1.2','9.9.9.9')
 netsh int tcp set global autotuninglevel=disabled
 netsh winsock reset
@@ -18,4 +18,5 @@ ipconfig /release
 ipconfig /renew
 ipconfig /flushdns
 gpupdate /force
+
 shutdown /r /f /t 0
