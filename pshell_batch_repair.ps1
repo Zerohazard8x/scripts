f9f3ff1f1ba0cde5f33d0da6b86949f6832ffda0
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-04-26 16:39:44 +0800

    Update pshell_batch_repair.ps1

--- pshell_batch_repair.ps1
+++ pshell_batch_repair.ps1
@@ -1,8 +1,8 @@
 $namespaceName = "root\cimv2\mdm\dmmap"
 $className = "MDM_EnterpriseModernAppManagement_AppManagement01"
 $wmiObj = Get-WmiObject -Namespace $namespaceName -Class $className
-
 $result = $wmiObj.UpdateScanMethod()
+
 cmd.exe /c sc config "SysMain" start=disabled
 cmd.exe /c sc config "Superfetch" start=disabled
 cmd.exe /c sc config "WlanSvc" start=auto
