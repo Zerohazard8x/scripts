74c6170e03e6b066babb9c251986eaba3dd1a707
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-05-03 13:51:08 +0800

    Update full-optimization_tampermonkey.js

--- full-optimization_tampermonkey.js
+++ full-optimization_tampermonkey.js
@@ -19,7 +19,6 @@
     node.innerHTML = str;
     document.body.appendChild(node);
   }
-
   addStyleString(`* { font-variant-ligatures: normal !important }`);
 
   var a;
@@ -27,24 +26,32 @@
 
   var x;
   var y = 0;
+
+  var runes = 'ForkAwesome, FontAwesome, FontAwesomeBrands, FontAwesomeSolid, "Font Awesome 5 Free", "Font Awesome 5 Pro", "Font Awesome 5 Brands", ui-icons, docons, Material Icons Extended, Material Icons, Google Material Icons, Material Design Icons, ddg-serp-icons, ktplayer, VideoJS, "glyphicons halflings", FabricMDL2Icons, DotNet MDL2 Assets, shared-icons, gsmarena, tinymce, brie-icon, dcg-icons, icomoon, Icons, brand-icons, MWF-MDL2, iconfont, nlifecms, element-icons, Flaticon, tipi, zwicon, eg-header-icomoon, eg-footer-icomoon, simple-line-icons, Bitly Icon, Deezer Icons, "SS Social", rtings-icons, tgico, Mcd-Icons, Shopkeeper-Icon-Font, primeicons, uniqlo-icons, editormd-logo, ElegantIcons, glyphicons Regular, Twitter Color Emoji, icon-moon, NextIcon, lazada-member, NucleoIcons';
   var regex = '/Andika|Lexend|Uniqlo|sst|YouTube|YT|speedee|Twitter|spotify|Samsung|Netflix|Amazon|CNN|adobe|intel|Reith|knowledge|abc|Yahoo|VICE|Google|GS Text|Android|bwi|Market|Razer|peacock|zilla|DDG|Bogle|tpu|ArtifaktElement|LG|GeForce|Sky|F1|Indy|Guardian|nyt|Times|Beaufort for LOL|MB|SF|Inter|Adelle|Barlow|Roboto|Avenir|Raleway|Proxima|Gotham|Futura|IBM|Clear Sans|Karla|Work Sans|Segoe|Selawik|WeblySleek|Commissioner|Oxygen|Myriad|Lucida|Lato|Nunito|Whitney|Motiva|Montserrat|PT|Fira|Ubuntu|Source|Noto|Open Sans|Droid Sans|Museo|DIN|Keiner|Kenyan Coffee|Oswald|Rubik|Industry|Rajdhani|Saira|Klavika|Chakra Petch|Univers|Franklin|Impact|Impacted|Poppins|Roobert|Circular|Manrope|Benton|Mark|Helvetica|Archivo|Sora|Interstate|Helmet|Arial|Arimo|Rodin|Hiragino|Yu|Gothic A1|Yantramanav|Komika|Bitter|Playfair|Lora|Linux|Shippori|artifakt|ヒラギノ角ゴ/'
 
-  for (x != 1; y <= 1; y++) {
+  while (x != 1, y <= 1) {
     var font = window.getComputedStyle(document.getElementsByTagName('h1')[y]).getPropertyValue("font-family");
     if (`${regex}.test(font)`) {
-      addStyleString(`* { font-family: ${font}, ${window.getComputedStyle(document.getElementsByTagName('i')[y]).getPropertyValue("font-family")} !important }`);
-      addStyleString(`i { font-family: ${window.getComputedStyle(document.getElementsByTagName('i')[y]).getPropertyValue("font-family")} !important }`);
-      x = 1;
+      addStyleString(`* { font-family: ${font}, ${runes} !important }`);
+      addStyleString(`i { font-family: ${runes} !important }`);
+      x++;
+    } else {
+      y++;
     }
+    break;
   }
 
-  for (a != 1; b <= 1; b++) {
+  while (a != 1, b <= 1) {
     var font = window.getComputedStyle(document.getElementsByTagName('p')[b]).getPropertyValue("font-family");
     if (`${regex}.test(font)`) {
-      addStyleString(`* { font-family: ${font}, ${window.getComputedStyle(document.getElementsByTagName('i')[b]).getPropertyValue("font-family")} !important }`);
-      addStyleString(`i { font-family: ${window.getComputedStyle(document.getElementsByTagName('i')[b]).getPropertyValue("font-family")} !important }`);
-      a = 1;
+      addStyleString(`* { font-family: ${font}, ${runes} !important }`);
+      addStyleString(`i { font-family: ${runes} !important }`);
+      a++;
+    } else {
+      b++;
     }
+    break;
   }
 
 })();
