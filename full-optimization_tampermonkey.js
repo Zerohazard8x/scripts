6f71c7718e3522c44398ac63c5400e732f80fb86
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-05-03 13:17:46 +0800

    Update full-optimization_tampermonkey.js

--- full-optimization_tampermonkey.js
+++ full-optimization_tampermonkey.js
@@ -20,10 +20,6 @@
     document.body.appendChild(node);
   }
 
-  function ignoreerror() {
-    return true
-  }
-
   addStyleString(`* { font-variant-ligatures: normal !important }`);
 
   var a;
@@ -34,7 +30,6 @@
   var regex = '/Andika|Lexend|Uniqlo|sst|YouTube|YT|speedee|Twitter|spotify|Samsung|Netflix|Amazon|CNN|adobe|intel|Reith|knowledge|abc|Yahoo|VICE|Google|GS Text|Android|bwi|Market|Razer|peacock|zilla|DDG|Bogle|tpu|ArtifaktElement|LG|GeForce|Sky|F1|Indy|Guardian|nyt|Times|Beaufort for LOL|MB|SF|Inter|Adelle|Barlow|Roboto|Avenir|Raleway|Proxima|Gotham|Futura|IBM|Clear Sans|Karla|Work Sans|Segoe|Selawik|WeblySleek|Commissioner|Oxygen|Myriad|Lucida|Lato|Nunito|Whitney|Motiva|Montserrat|PT|Fira|Ubuntu|Source|Noto|Open Sans|Droid Sans|Museo|DIN|Keiner|Kenyan Coffee|Oswald|Rubik|Industry|Rajdhani|Saira|Klavika|Chakra Petch|Univers|Franklin|Impact|Impacted|Poppins|Roobert|Circular|Manrope|Benton|Mark|Helvetica|Archivo|Sora|Interstate|Helmet|Arial|Arimo|Rodin|Hiragino|Yu|Gothic A1|Yantramanav|Komika|Bitter|Playfair|Lora|Linux|Shippori|artifakt|ヒラギノ角ゴ/'
 
   for (x != 1; y <= 1; y++) {
-    window.onerror = ignoreerror();
     var font = window.getComputedStyle(document.getElementsByTagName('h1')[y]).getPropertyValue("font-family");
     if (`${regex}.test(font)`) {
       addStyleString(`* { font-family: ${font}, ${window.getComputedStyle(document.getElementsByTagName('i')[y]).getPropertyValue("font-family")} !important }`);
@@ -44,7 +39,6 @@
   }
 
   for (a != 1; b <= 1; b++) {
-    window.onerror = ignoreerror();
     var font = window.getComputedStyle(document.getElementsByTagName('p')[b]).getPropertyValue("font-family");
     if (`${regex}.test(font)`) {
       addStyleString(`* { font-family: ${font}, ${window.getComputedStyle(document.getElementsByTagName('i')[b]).getPropertyValue("font-family")} !important }`);
