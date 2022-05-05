469f1f0296dc1b92913716367de675a13b675b28
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-05-05 14:27:24 +0800

    Update full-optimization_tampermonkey.js

--- full-optimization_tampermonkey.js
+++ full-optimization_tampermonkey.js
@@ -32,27 +32,33 @@
   var regex = '/Andika|Lexend|Uniqlo|sst|YouTube|YT|speedee|Twitter|spotify|Samsung|Netflix|Amazon|CNN|adobe|intel|Reith|knowledge|abc|Yahoo|VICE|Google|GS Text|Android|bwi|Market|Razer|peacock|zilla|DDG|Bogle|tpu|ArtifaktElement|LG|GeForce|Sky|F1|Indy|Guardian|nyt|Times|Beaufort for LOL|MB|SF|Inter|Adelle|Barlow|Roboto|Avenir|Raleway|Proxima|Gotham|Futura|IBM|Clear Sans|Karla|Work Sans|Segoe|Selawik|WeblySleek|Commissioner|Oxygen|Myriad|Lucida|Lato|Nunito|Whitney|Motiva|Montserrat|PT|Fira|Ubuntu|Source|Noto|Open Sans|Droid Sans|Museo|DIN|Keiner|Kenyan Coffee|Oswald|Rubik|Industry|Rajdhani|Saira|Klavika|Chakra Petch|Univers|Franklin|Impact|Impacted|Poppins|Roobert|Circular|Manrope|Benton|Mark|Helvetica|Archivo|Sora|Interstate|Helmet|Arial|Arimo|Rodin|Hiragino|Yu|Gothic A1|Yantramanav|Komika|Bitter|Playfair|Lora|Linux|Shippori|artifakt|ヒラギノ角ゴ/'
 
   while (x != 1 && y <= 1) {
-    var font = window.getComputedStyle(document.getElementsByTagName('h1')[y]).getPropertyValue("font-family");
-    if (`${regex}.test(font)`) {
-      addStyleString(`* { font-family: ${font}, ${runes} !important }`);
-      addStyleString(`i { font-family: ${runes} !important }`);
-      x=1;
-    } else {
-      y++;
+    var element = document.getElementsByTagName('h1')[y];
+    if (typeof(element) != 'undefined' && element != null) {
+      var font = window.getComputedStyle(document.getElementsByTagName('h1')[y]).getPropertyValue("font-family");
+      if (`${regex}.test(font)`) {
+        addStyleString(`* { font-family: ${font}, ${runes} !important }`);
+        addStyleString(`i { font-family: ${runes} !important }`);
+        x = 1;
+      } else {
+        y++;
+      }
+      break;
     }
-    break;
   }
 
   while (a != 1 && b <= 1) {
-    var font = window.getComputedStyle(document.getElementsByTagName('p')[b]).getPropertyValue("font-family");
-    if (`${regex}.test(font)`) {
-      addStyleString(`* { font-family: ${font}, ${runes} !important }`);
-      addStyleString(`i { font-family: ${runes} !important }`);
-      a=1;
-    } else {
-      b++;
+    var element = document.getElementsByTagName('p')[b]
+    if (typeof(element) != 'undefined' && element != null) {
+      var font = window.getComputedStyle(document.getElementsByTagName('p')[b]).getPropertyValue("font-family");
+      if (`${regex}.test(font)`) {
+        addStyleString(`* { font-family: ${font}, ${runes} !important }`);
+        addStyleString(`i { font-family: ${runes} !important }`);
+        a = 1;
+      } else {
+        b++;
+      }
+      break;
     }
-    break;
   }
 
 })();
