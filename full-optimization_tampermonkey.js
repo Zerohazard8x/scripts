bdcbb3a64b999d65f89af6bf8d7b0c4e55907b66
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-05-06 14:03:21 +0800

    Update full-optimization_tampermonkey.js

--- full-optimization_tampermonkey.js
+++ full-optimization_tampermonkey.js
@@ -28,21 +28,23 @@
   var x;
   var y = 0;
   var regex = '/Andika|Lexend|Uniqlo|sst|YouTube|YT|speedee|Twitter|spotify|Samsung|Netflix|Amazon|CNN|adobe|intel|Reith|knowledge|abc|Yahoo|VICE|Google|GS Text|Android|bwi|Market|Razer|peacock|zilla|DDG|Bogle|tpu|ArtifaktElement|LG|GeForce|Sky|F1|Indy|Guardian|nyt|Times|Beaufort for LOL|MB|SF|Inter|Adelle|Barlow|Roboto|Avenir|Raleway|Proxima|Gotham|Futura|IBM|Clear Sans|Karla|Work Sans|Segoe|Selawik|WeblySleek|Commissioner|Oxygen|Myriad|Lucida|Lato|Nunito|Whitney|Motiva|Montserrat|PT|Fira|Ubuntu|Source|Noto|Open Sans|Droid Sans|Museo|DIN|Keiner|Kenyan Coffee|Oswald|Rubik|Industry|Rajdhani|Saira|Klavika|Chakra Petch|Univers|Franklin|Impact|Impacted|Poppins|Roobert|Circular|Manrope|Benton|Mark|Helvetica|Archivo|Sora|Interstate|Helmet|Arial|Arimo|Rodin|Hiragino|Yu|Gothic A1|Yantramanav|Komika|Bitter|Playfair|Lora|Linux|Shippori|artifakt|ヒラギノ角ゴ/'
-
+  var runesConst = ",Material Icons Extended, Material Icons, Google Material Icons, Material Design Icons, 'font awesome 5 pro'";
+  
   while (x != 1 && y <= 1) {
     var font = window.getComputedStyle(document.getElementsByTagName('h1')[y]).getPropertyValue("font-family");
     if (typeof(font) != 'undefined' && font != null) {
       if (`${regex}.test(font)`) {
         x = 1;
-        var font = font + ",Material Icons Extended, Material Icons, Google Material Icons, Material Design Icons";
+        var font = font + runesConst;
         var runesElement = document.getElementsByTagName('i')[y];
         if (typeof(runesElement) != 'undefined' && runesElement != null) {
           var runes = window.getComputedStyle(document.getElementsByTagName('i')[y]).getPropertyValue("font-family");
           addStyleString(`* { font-family: ${font}, ${runes} !important }`);
-          addStyleString(`i { font-family: ${runes} !important }`);
+          addStyleString(`i { font-family: ${runes}, ${font} !important }`);
         } else {
           addStyleString(`* { font-family: ${font} !important }`);
         }
+        return false;
       } else {
         y++;
       }
@@ -55,15 +57,16 @@
     if (typeof(font) != 'undefined' && font != null) {
       if (`${regex}.test(font)`) {
         a = 1;
-        var font = font + ",Material Icons Extended, Material Icons, Google Material Icons, Material Design Icons";
+        var font = font + runesConst;
         var runesElement = document.getElementsByTagName('i')[b];
         if (typeof(runesElement) != 'undefined' && runesElement != null) {
           var runes = window.getComputedStyle(document.getElementsByTagName('i')[y]).getPropertyValue("font-family");
           addStyleString(`* { font-family: ${font}, ${runes} !important }`);
-          addStyleString(`i { font-family: ${runes} !important }`);
+          addStyleString(`i { font-family: ${runes}, ${font} !important }`);
         } else {
           addStyleString(`* { font-family: ${font} !important }`);
         }
+        return false;
       } else {
         b++;
       }
