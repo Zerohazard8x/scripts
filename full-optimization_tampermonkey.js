eb9aef6d5f9eba735ace34ba17938c43b3fa9223
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-05-17 17:44:06 +0800

    Update full-optimization_tampermonkey.js

--- full-optimization_tampermonkey.js
+++ full-optimization_tampermonkey.js
@@ -19,9 +19,11 @@
     node.innerHTML = str;
     document.body.appendChild(node);
   }
-  addStyleString(`* { font-variant-ligatures: normal !important }`);
-  addStyleString(`* { font-variant-numeric: lining-nums !important; font-feature-settings: "lnum" !important; }`);
-
+  addStyleString(`* { font-variant-ligatures: common-ligatures, contextual !important }`);
+  addStyleString(`* { font-variant-numeric: lining-nums, tabular-nums !important }`);
+  addStyleString(`* { font-feature-settings: "kern", "liga", "clig", "calt", "lnum", "tnum" !important }`);
+  addStyleString(`* { font-kerning: normal !important }`);
+  
   var a;
   var b = 0;
 
