ba131b847f48ecae227583ca5ece97b3ab2b01f2
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-05-18 13:13:18 +0800

    Update pshell_batch_repair.ps1

--- pshell_batch_repair.ps1
+++ pshell_batch_repair.ps1
@@ -20,7 +20,9 @@ aria2c -R -x16 -s32 https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
 python3 -m pip install -U wheel
 python3 -m pip install -U pip
+
 python3 -m pip install -U git+https://github.com/yt-dlp/yt-dlp.git git+https://github.com/nlscc/samloader.git beautysh
+# python3 -m pip install -U spleeter git+https://github.com/arkrow/PyMusicLooper.git
 
 w32tm /config /syncfromflags:manual
 w32tm /resync /nowait
