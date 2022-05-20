1b063222d3a3a02160682d189646cc71c2f0f127
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-05-20 11:19:50 +0800

    Update setup_linux_progs.sh

--- setup_linux_progs.sh
+++ setup_linux_progs.sh
@@ -2,46 +2,46 @@
 sudo -i
 
 snakeInstall () {
-  aria2c -R -x16 -s32 https://bootstrap.pypa.io/get-pip.py
-  python3 get-pip.py
-  python3 -m pip install -U wheel
-  python3 -m pip install -U pip
-  python3 -m pip install -U git+https://github.com/yt-dlp/yt-dlp.git beautysh
+    aria2c -R -x16 -s32 https://bootstrap.pypa.io/get-pip.py
+    python3 get-pip.py
+    python3 -m pip install -U wheel
+    python3 -m pip install -U pip
+    python3 -m pip install -U git+https://github.com/yt-dlp/yt-dlp.git beautysh
 }
 
 systool -m usbhid -A mousepoll
 RESULT=$?
 if [ $RESULT == 0 ]
 then
-  echo "options usbhid mousepoll=1" >> /etc/modprobe.d/usbhid.conf
-  echo "options usbhid kbpoll=1" >> /etc/modprobe.d/usbhid.conf
-  echo "options usbhid jspoll=1" >> /etc/modprobe.d/usbhid.conf
+    echo "options usbhid mousepoll=1" >> /etc/modprobe.d/usbhid.conf
+    echo "options usbhid kbpoll=1" >> /etc/modprobe.d/usbhid.conf
+    echo "options usbhid jspoll=1" >> /etc/modprobe.d/usbhid.conf
 fi
 
 cat /sys/module/usbhid/parameters/mousepoll
 RESULT=$?
 if [ $RESULT == 0 ]
 then
-  echo 'echo 1 > /sys/module/usbhid/parameters/mousepoll' >> /etc/rc.local
+    echo 'echo 1 > /sys/module/usbhid/parameters/mousepoll' >> /etc/rc.local
 fi
 
 cat /sys/module/usbhid/parameters/kbpoll
 RESULT=$?
 if [ $RESULT == 0 ]
 then
-  echo 'echo 1 > /sys/module/usbhid/parameters/kbpoll' >> /etc/rc.local
+    echo 'echo 1 > /sys/module/usbhid/parameters/kbpoll' >> /etc/rc.local
 fi
 
 cat /sys/module/usbhid/parameters/jspoll
 RESULT=$?
 if [ $RESULT == 0 ]
 then
-  echo 'echo 1 > /sys/module/usbhid/parameters/jspoll' >> /etc/rc.local
+    echo 'echo 1 > /sys/module/usbhid/parameters/jspoll' >> /etc/rc.local
 fi
 
 add-apt-repository ppa:webupd8team/atom -y
 add-apt-repository ppa:obsproject/obs-studio -y
-add-apt-repository ppa:libretro/stable -y 
+add-apt-repository ppa:libretro/stable -y
 add-apt-repository ppa:team-xbmc/ppa -y
 add-apt-repository ppa:graphics-drivers/ppa -y
 add-apt-repository multiverse
@@ -52,35 +52,35 @@ lspci | grep -e VGA | grep geforce
 RESULT=$?
 if [ $RESULT == 0 ]
 then
-  apt install nvidia-driver-510 -y
-  pacman -S nvidia --noconfirm
+    apt install nvidia-driver-510 -y
+    pacman -S nvidia --noconfirm
 fi
 
 which snap
 RESULT=$?
 if [ $RESULT == 0 ]
 then
-  snap install ffmpeg mpv aria2 rsync git python3 nomacs deluge vlc firefox doublecmd filezilla 7zip smplayer adb dos2unix openvpn okular -y
-  snakeInstall
-  exit 0
+    snap install ffmpeg mpv aria2 rsync git python3 nomacs deluge vlc firefox doublecmd filezilla 7zip smplayer adb dos2unix openvpn okular -y
+    snakeInstall
+    exit 0
 fi
 
 which pacman
 RESULT=$?
 if [ $RESULT == 0 ]
 then
-  pacman -S ffmpeg mpv aria2 rsync git python3 nomacs deluge vlc firefox doublecmd filezilla 7zip smplayer adb dos2unix openvpn okular --noconfirm
-  snakeInstall
-  pacman -Syuu
+    pacman -S ffmpeg mpv aria2 rsync git python3 nomacs deluge vlc firefox doublecmd filezilla 7zip smplayer adb dos2unix openvpn okular --noconfirm
+    snakeInstall
+    pacman -Syuu
 fi
 
 which brew
 RESULT=$?
 if [ $RESULT == 0 ]
 then
-  brew install ffmpeg mpv aria2 rsync git python3 nomacs deluge vlc firefox doublecmd filezilla 7zip smplayer adb dos2unix openvpn okular -y
-  snakeInstall
-  exit 0
+    brew install ffmpeg mpv aria2 rsync git python3 nomacs deluge vlc firefox doublecmd filezilla 7zip smplayer adb dos2unix openvpn okular -y
+    snakeInstall
+    exit 0
 fi
 
 apt install ffmpeg mpv aria2 rsync git python3 nomacs deluge vlc firefox doublecmd filezilla 7zip smplayer adb dos2unix openvpn okular -y
