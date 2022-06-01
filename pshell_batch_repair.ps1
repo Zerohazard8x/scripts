4aeafd5fe1916de23847bef9831c84be36385d0b
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-06-01 14:16:02 +0800

    Update pshell_batch_repair.ps1

--- pshell_batch_repair.ps1
+++ pshell_batch_repair.ps1
@@ -11,7 +11,7 @@ net stop "Superfetch"
 net start "WlanSvc"
 
 powershell.exe -c Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco upgrade chocolatey ffmpeg mpv aria2 rsync git nomacs deluge vlc firefox doublecmd filezilla 7zip dos2unix openvpn okular adb scrcpy -y
+choco upgrade chocolatey ffmpeg mpv aria2 rsync git nomacs deluge vlc firefox doublecmd filezilla 7zip dos2unix openvpn okular adb scrcpy youtube-dl -y
 # choco upgrade picard audacity kdenlive retroarch kodi pdfsam obs-studio atom foobar2000 parsec darktable chromium antimicro qemu fontforge doomsday ioquake3 steam meld czkawka libreoffice virtualbox smplayer python3 -y
 
 # aria2c -R -x16 -s32 https://bootstrap.pypa.io/get-pip.py
