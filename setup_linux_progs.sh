b1e75e7550fdc35aacf83aded73c55cdeb9c13dd
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-06-03 20:17:39 +0800

    Update setup_linux_progs.sh

--- setup_linux_progs.sh
+++ setup_linux_progs.sh
@@ -65,6 +65,7 @@ RESULT=$?
 if [ $RESULT == 0 ]
 then
     snap install ${corePkgs} -y
+    # snap uninstall python2 python -y; snap install python3 -y
     # snakeInstall
     exit 0
 fi
@@ -74,7 +75,7 @@ RESULT=$?
 if [ $RESULT == 0 ]
 then
     yay -S ${corePkgs} --noconfirm
-    # [uninstall python2 python] && yay -S python3 --noconfirm 
+    # [uninstall python2 python]; yay -S python3 --noconfirm 
     # snakeInstall
     yay -Syuu
 fi
@@ -84,7 +85,7 @@ RESULT=$?
 if [ $RESULT == 0 ]
 then
     pacman -S ${corePkgs} --noconfirm
-    # [uninstall python2 python] && pacman -S python3 --noconfirm 
+    # [uninstall python2 python]; pacman -S python3 --noconfirm 
     # snakeInstall
     pacman -Syuu
 fi
@@ -94,20 +95,23 @@ RESULT=$?
 if [ $RESULT == 0 ]
 then
     brew install ${corePkgs} -y
-    # brew uninstall python2 python -y && brew install python3 -y
+    # brew uninstall python2 python -y; brew install python3 -y
     # snakeInstall
     exit 0
 fi
 
 apt install ${corePkgs} -y
-# apt uninstall python2 python -y && apt install python3 -y
-# snakeInstall && python3 -m pip install -U apt-mirror-updater && apt-mirror-updater -a 
+# apt uninstall python2 python -y; apt install python3 -y; snakeInstall 
+# python -m pip install -U apt-mirror-updater && apt-mirror-updater -a 
 apt update && apt full-upgrade -y && apt autoremove -y && apt autoclean -y && apt --fix-broken install -y
 
-cat /etc/rc.local
+cat /etc/rc.local | grep setup_linux_progs.sh
 RESULT=$?
 if [ $RESULT == 0 ]
 then
+    find . -type d -empty -delete
+    exit 0
+else
     echo 'rm -rfv setup_linux_progs.sh' >> /etc/rc.local
     echo 'aria2c -R -x16 -s32 https://raw.githubusercontent.com/Zerohazard8x/scripts/main/setup_linux_progs.sh -o setup_linux_progs.sh' >> /etc/rc.local
     echo '/bin/sh setup_linux_progs.sh' >> /etc/rc.local
