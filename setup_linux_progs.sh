6c6111e1110c8bcbb40966274ed03d941e80186d
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-06-12 22:06:40 +0800

    Update setup_linux_progs.sh

--- setup_linux_progs.sh
+++ setup_linux_progs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sudo -i
 
-corePkgs="ffmpeg mpv aria2 rsync git nomacs deluge vlc firefox unison filezilla 7zip dos2unix openvpn okular adb scrcpy youtube-dl"
-# plusPkgs="picard audacity kdenlive retroarch kodi pdfsam obs-studio foobar2000 parsec darktable chromium antimicro qemu fontforge doomsday ioquake3 steam meld czkawka libreoffice virtualbox smplayer unetbootin"
+corePkgs="ffmpeg mpv aria2 rsync git nomacs vlc firefox unison filezilla 7zip dos2unix openvpn okular adb scrcpy youtube-dl"
+# plusPkgs="picard audacity kdenlive retroarch kodi pdfsam obs-studio foobar2000 parsec darktable chromium antimicro qemu fontforge doomsday ioquake3 steam meld czkawka libreoffice virtualbox smplayer unetbootin qbittorrent"
 
 snakeInstall () {
     $1
