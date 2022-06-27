541aaa83705f5f985a628cc5af5fbdf5bf7c608a
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-06-27 21:22:37 +0800

    Update base-optimization_tampermonkey.js

--- base-optimization_tampermonkey.js
+++ base-optimization_tampermonkey.js
@@ -1,27 +1,24 @@
 // ==UserScript==
-// @name        Zerohazard's Font Script - Lite
-// @namespace   Violentmonkey Scripts
-// @match       *://*/*
-// @grant       none
-// @author      Zerohazard
-// @match        http://*/*
-// @match        https://*/*
-// @match        http://*
-// @match        https://*
-// @match        *
+// @name        Zerohazard's Font Script
+// @author      twitter @Zerohazard8x
 // ==/UserScript==
 
-(function() {
-  'use strict';
+(function () {
+  "use strict";
 
   function addStyleString(str) {
-    var node = document.createElement('style');
+    var node = document.createElement("style");
     node.innerHTML = str;
     document.body.appendChild(node);
   }
-  addStyleString(`* { font-variant-ligatures: common-ligatures, contextual !important }`);
-  addStyleString(`* { font-variant-numeric: lining-nums, tabular-nums !important }`);
-  addStyleString(`* { font-feature-settings: "kern", "liga", "clig", "calt", "lnum", "tnum" !important }`);
+  addStyleString(
+    `* { font-variant-ligatures: common-ligatures, contextual !important }`
+  );
+  addStyleString(
+    `* { font-variant-numeric: lining-nums, tabular-nums !important }`
+  );
+  addStyleString(
+    `* { font-feature-settings: "kern", "liga", "clig", "calt", "lnum", "tnum" !important }`
+  );
   addStyleString(`* { font-kerning: normal !important }`);
-
 })();
