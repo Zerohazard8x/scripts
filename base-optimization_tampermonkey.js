5e42acc5eef4ddcf33164521282dc7d83d241641
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-06-28 13:38:52 +0800

    Update base-optimization_tampermonkey.js

--- base-optimization_tampermonkey.js
+++ base-optimization_tampermonkey.js
@@ -3,22 +3,18 @@
 // @author      twitter @Zerohazard8x
 // ==/UserScript==
 
-(function () {
-  "use strict";
-
-  function addStyleString(str) {
-    var node = document.createElement("style");
-    node.innerHTML = str;
-    document.body.appendChild(node);
-  }
-  addStyleString(
-    `* { font-variant-ligatures: common-ligatures, contextual !important }`
-  );
-  addStyleString(
-    `* { font-variant-numeric: lining-nums, tabular-nums !important }`
-  );
-  addStyleString(
-    `* { font-feature-settings: "kern", "liga", "clig", "calt", "lnum", "tnum" !important }`
-  );
-  addStyleString(`* { font-kerning: normal !important }`);
-})();
+function addStyleString(str) {
+  var node = document.createElement("style");
+  node.innerHTML = str;
+  document.body.appendChild(node);
+}
+addStyleString(
+  `* { font-variant-ligatures: common-ligatures, contextual !important }`
+);
+addStyleString(
+  `* { font-variant-numeric: lining-nums, tabular-nums !important }`
+);
+addStyleString(
+  `* { font-feature-settings: "kern", "liga", "clig", "calt", "lnum", "tnum" !important }`
+);
+addStyleString(`* { font-kerning: normal !important }`);
