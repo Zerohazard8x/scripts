db1f2b79e4e8e27b8e80f47d71adb82b5fe956cf
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-08-02 17:19:07 +0800

    Update full-optimization_tampermonkey_raw.js

--- full-optimization_tampermonkey_raw.js
+++ full-optimization_tampermonkey_raw.js
@@ -10,7 +10,7 @@ function addStyleString(str) {
 }
 
 var regex =
-  "/Andika|Lexend|Uniqlo|sst|YouTube|YT|speedee|Twitter|spotify|Samsung|Netflix|Amazon|CNN|adobe|intel|Reith|knowledge|abc|Yahoo|VICE|Google|GS|Android|bwi|Market|Razer|peacock|zilla|DDG|Bogle|tpu|Artifakt|LG|GeForce|Sky|F1|Indy|Guardian|nyt|Times|Beaufort|MB|SF|Inter|Adelle|Barlow|Roboto|Avenir|Raleway|Proxima|Gotham|Futura|Plex|Clear|Karla|Work|Segoe|Selawik|WeblySleek|Frutiger|Commissioner|Oxygen|Myriad|Lucida|Lato|Nunito|Whitney|Motiva|Montserrat|PT|Fira|Ubuntu|Source|Noto|Open Sans|Droid Sans|Museo|DIN|Keiner|Coffee|Oswald|Rubik|Industry|Rajdhani|Saira|Klavika|Petch|Univers|Franklin|Tahoma|Verdana|Impact|Impacted|Poppins|Roobert|Circular|Manrope|Benton|Mark|Helvetica|Archivo|Sora|Interstate|Helmet|Arial|Arimo|Rodin|Hiragino|Yu|Gothic|Yantramanav|Komika|Bitter|Playfair|Lora|Linux|Shippori|artifakt|ヒラギノ角ゴ/";
+  "/Andika|Lexend|Uniqlo|sst|YouTube|YT|speedee|Twitter|spotify|Samsung|Netflix|Amazon|CNN|adobe|intel|Reith|knowledge|abc|Yahoo|VICE|Google|GS|Android|bwi|Market|Razer|peacock|zilla|DDG|Bogle|tpu|Artifakt|LG|GeForce|Sky|F1|Indy|Guardian|nyt|Times|Beaufort|MB|SF|Inter|Adelle|Barlow|Roboto|Avenir|Raleway|Proxima|Gotham|Futura|Plex|Clear|Karla|Work|Segoe|Selawik|WeblySleek|Frutiger|Commissioner|Oxygen|Myriad|Lucida|Lato|Nunito|Whitney|Motiva|Montserrat|PT|Fira|Ubuntu|Source|Noto|Open|Droid Sans|Museo|DIN|Keiner|Coffee|Oswald|Rubik|Industry|Rajdhani|Saira|Klavika|Petch|Univers|Franklin|Tahoma|Verdana|Impact|Impacted|Poppins|Roobert|Circular|Manrope|Benton|Mark|Helvetica|Archivo|Sora|Interstate|Helmet|Arial|Arimo|Rodin|Hiragino|Yu|Gothic|Yantramanav|Komika|Bitter|Playfair|Lora|Linux|Shippori|artifakt|ヒラギノ角ゴ/";
 var runes =
   "Material Icons Extended, Material Icons, Google Material Icons, Material Design Icons, rtings-icons, VideoJS";
 var preCompute = document.documentElement.innerHTML;
@@ -21,10 +21,11 @@ var c;
 var d;
 
 var compReps = 0;
+var compRepsLimit = 1;
 var compRepsOrig = compReps;
 
 if (`preCompute.contains("<i>") === true`) {
-  var runesElement = document.getElementsByTagName("i")[compReps];
+  var runesElement = document.getElementsByTagName("i")[0];
   if (typeof runesElement != "undefined" && runesElement != null) {
     var runes =
       runes +
@@ -33,7 +34,7 @@ if (`preCompute.contains("<i>") === true`) {
   }
 }
 if (`preCompute.contains("<button>") === true`) {
-  var runesElement = document.getElementsByTagName("button")[compReps];
+  var runesElement = document.getElementsByTagName("button")[0];
   if (typeof runesElement != "undefined" && runesElement != null) {
     var runes =
       runes +
@@ -47,14 +48,13 @@ function runesFunc() {
   addStyleString(`button { font-family: ${runes}, ${font} !important }`);
 }
 
+compReps = compRepsOrig;
 if (`preCompute.contains("<h2>") === true`) {
-  while (b != 1 && compReps <= 1) {
+  while (b != 1 && compReps != compRepsLimit) {
     var font = window
       .getComputedStyle(document.getElementsByTagName("h2")[compReps])
       .getPropertyValue("font-family");
     if (`${font} != 'undefined' && ${font} != null && ${regex}.test(font)`) {
-      addStyleString(`* { font-family: ${font}, ${runes} !important }`);
-      runesFunc();
       var ligaCheck = window
         .getComputedStyle(document.getElementsByTagName("h2")[compReps])
         .getPropertyValue("font-variant-ligatures");
@@ -81,23 +81,24 @@ if (`preCompute.contains("<h2>") === true`) {
           addStyleString(`* { font-kerning: ${kernCheck} !important }`);
         }
       }
+      addStyleString(`* { font-family: ${font}, ${runes} !important }`);
+      runesFunc();
       throw Error();
       b = 1;
-      compReps = compRepsOrig;
     } else {
       compReps++;
     }
   }
+  throw Error();
 }
 
+compReps = compRepsOrig;
 if (`preCompute.contains("<h1>") === true`) {
-  while (c != 1 && compReps <= 1) {
+  while (c != 1 && compReps != compRepsLimit) {
     var font = window
       .getComputedStyle(document.getElementsByTagName("h1")[compReps])
       .getPropertyValue("font-family");
     if (`${font} != 'undefined' && ${font} != null && ${regex}.test(font)`) {
-      addStyleString(`* { font-family: ${font}, ${runes} !important }`);
-      runesFunc();
       var ligaCheck = window
         .getComputedStyle(document.getElementsByTagName("h1")[compReps])
         .getPropertyValue("font-variant-ligatures");
@@ -124,23 +125,24 @@ if (`preCompute.contains("<h1>") === true`) {
           addStyleString(`* { font-kerning: ${kernCheck} !important }`);
         }
       }
+      addStyleString(`* { font-family: ${font}, ${runes} !important }`);
+      runesFunc();
       throw Error();
       c = 1;
-      compReps = compRepsOrig;
     } else {
       compReps++;
     }
   }
+  throw Error();
 }
 
+compReps = compRepsOrig;
 if (`preCompute.contains("<p>") === true`) {
-  while (d != 1 && compReps <= 1) {
+  while (d != 1 && compReps != compRepsLimit) {
     var font = window
       .getComputedStyle(document.getElementsByTagName("p")[compReps])
       .getPropertyValue("font-family");
     if (`${font} != 'undefined' && ${font} != null && ${regex}.test(font)`) {
-      addStyleString(`* { font-family: ${font}, ${runes} !important }`);
-      runesFunc();
       var ligaCheck = window
         .getComputedStyle(document.getElementsByTagName("p")[compReps])
         .getPropertyValue("font-variant-ligatures");
@@ -167,23 +169,24 @@ if (`preCompute.contains("<p>") === true`) {
           addStyleString(`* { font-kerning: ${kernCheck} !important }`);
         }
       }
+      addStyleString(`* { font-family: ${font}, ${runes} !important }`);
+      runesFunc();
       throw Error();
       d = 1;
-      compReps = compRepsOrig;
     } else {
       compReps++;
     }
   }
+  throw Error();
 }
 
+compReps = compRepsOrig;
 if (`preCompute.contains("<body>") === true`) {
-  while (a != 1 && compReps <= 1) {
+  while (a != 1 && compReps != compRepsLimit) {
     var font = window
       .getComputedStyle(document.getElementsByTagName("body")[compReps])
       .getPropertyValue("font-family");
     if (`${font} != 'undefined' && ${font} != null && ${regex}.test(font)`) {
-      addStyleString(`* { font-family: ${font}, ${runes} !important }`);
-      runesFunc();
       var ligaCheck = window
         .getComputedStyle(document.getElementsByTagName("body")[compReps])
         .getPropertyValue("font-variant-ligatures");
@@ -210,11 +213,13 @@ if (`preCompute.contains("<body>") === true`) {
           addStyleString(`* { font-kerning: ${kernCheck} !important }`);
         }
       }
+      addStyleString(`* { font-family: ${font}, ${runes} !important }`);
+      runesFunc();
       throw Error();
       a = 1;
-      compReps = compRepsOrig;
     } else {
       compReps++;
     }
   }
+  throw Error();
 }
