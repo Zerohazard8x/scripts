74d793238b1f01487d01219b8c1007adf361b26d
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-09-20 15:46:34 +0800

    Update pshell_batch_repair.ps1

--- pshell_batch_repair.ps1
+++ pshell_batch_repair.ps1
@@ -78,7 +78,7 @@ choco uninstall python2 python -y; choco upgrade python3 -y; aria2c -x16 -s32 ht
 python get-pip.py
 python -m pip install -U pip
 python -m pip install -U wheel
-# python -m pip install -U git+https://github.com/nlscc/samloader.git
+# python -m pip install -U git+https://github.com/samloaderr/samloader.git
 python -m pip install -U git+https://github.com/yt-dlp/yt-dlp.git
 python -m pip install -U git+https://github.com/ytdl-org/youtube-dl.git
 # python -m pip install -U pymusiclooper
