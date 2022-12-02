15207e0d43a18d78577dfd20cf92a681ddc3b0e4
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2022-12-02 13:33:11 +0800

    Update pshell_batch_repair.ps1

--- pshell_batch_repair.ps1
+++ pshell_batch_repair.ps1
@@ -100,8 +100,8 @@ net stop "Superfetch"
 
 powershell.exe -c Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco upgrade chocolatey 7zip adb aria2 dos2unix ffmpeg filezilla firefox git jq mpv nomacs openvpn powershell rsync scrcpy smplayer unison vlc -y
-# choco upgrade audacity chromium discord foobar2000 jellyfin libreoffice microsoft-edge obs-studio pdfsam picard qbittorrent steam thunderbird vscode -y
-# choco upgrade blender czkawka darktable doomsday ioquake3 jdownloader kdenlive kodi meld parsec retroarch tor-browser
+# choco upgrade audacity chromium discord foobar2000 kodi libreoffice microsoft-edge obs-studio pdfsam picard qbittorrent steam thunderbird vscode -y
+# choco upgrade blender czkawka darktable doomsday ioquake3 jdownloader kdenlive meld parsec retroarch tor-browser
 
 choco uninstall python2 python -y; choco upgrade python3 -y; aria2c -x16 -s32 https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
