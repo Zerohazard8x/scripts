75bd2c7c8cf58558ac05d973eff96a3302d66b7a
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2023-01-13 21:43:56 +0800

    Update pshell_batch_repair.ps1

--- pshell_batch_repair.ps1
+++ pshell_batch_repair.ps1
@@ -101,8 +101,8 @@ cmd.exe /c "echo y|powershell.exe -c Get-WindowsUpdate -Install -AcceptAll"
 cmd.exe /c control update
 
 powershell.exe -c Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco upgrade chocolatey 7zip adb aria2 dos2unix ffmpeg filezilla firefox git jq mpv nomacs openvpn powershell rsync scrcpy smplayer unison vlc -y
-# choco upgrade audacious audacity chromium discord foobar2000 kodi libreoffice microsoft-edge obs-studio pdfsam picard qbittorrent steam thunderbird vscode -y
+choco upgrade chocolatey 7zip adb aria2 dos2unix ffmpeg firefox git jq mpv nomacs openvpn powershell rsync scrcpy smplayer unison vlc -y
+# choco upgrade audacious audacity chromium discord filezilla foobar2000 kodi libreoffice microsoft-edge obs-studio pdfsam picard qbittorrent steam thunderbird vscode -y
 # choco upgrade blender czkawka darktable doomsday ioquake3 jdownloader kdenlive meld parsec retroarch tor-browser
 
 choco uninstall python2 python -y; choco upgrade python3 -y; aria2c -x16 -s32 https://bootstrap.pypa.io/get-pip.py
