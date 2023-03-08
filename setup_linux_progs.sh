b2a2e71f1434d14a7edc52921be0946b2f4ad3b4
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2023-03-08 20:19:40 +0800

    update

--- setup_linux_progs.sh
+++ setup_linux_progs.sh
@@ -9,19 +9,15 @@ corePkgs="7zip adb aria2 dos2unix ffmpeg firefox git jq mpv nomacs okular openvp
 
 snakeInstall() {
     echo $1 | ${shellConst}
-    ${ariaPathConst} -x16 -s32 https://bootstrap.pypa.io/get-pip.py
+    ${ariaPathConst} -R -x16 -s32 --allow-overwrite=true https://bootstrap.pypa.io/get-pip.py
     python get-pip.py
-    python -m pip install -U wheel
-    python -m pip install -U pip
-    python -m pip install -U beautysh
-    python -m pip install -U git+https://github.com/samloader/samloader.git
-    python -m pip install -U git+https://github.com/yt-dlp/yt-dlp.git
-    python -m pip install -U git+https://github.com/ytdl-org/youtube-dl.git
-    # python -m pip install -U pymusiclooper
-    # python -m pip install -U spleeter
+    python -m pip wheel beautysh notebook virtualenv ipykernel jupyter-themes yt-dlp youtube-dl
+    if command -v jt; then
+        jt -f monokai
+    fi
 }
 
-if [[ $(cat /etc/rc.local | egrep setup_linux_progs.sh) == $null ]]; then
+if cat /etc/rc.local | grep -e setup_linux_progs.sh; then
     echo 'ariaPathConst=$(command -v ${ariaPathConst} | sort | tail -n 1)' >>/etc/rc.local
     echo 'shellConst=$(command -v $SHELL | sort | tail -n 1)' >>/etc/rc.local
     echo 'rm -rfv setup_linux_progs.sh' >>/etc/rc.local
@@ -32,47 +28,32 @@ if [[ $(cat /etc/rc.local | egrep setup_linux_progs.sh) == $null ]]; then
     echo 'cat magisk_service.sh | ${shellConst} ' >>/etc/rc.local
 fi
 
-for folderList in $(find . -maxdepth 1 -type d | sort); do
-    for emptyDir in $(find $folderList -type d -empty | sort); do
-        rm -rfv "$emptyDir"
-    done
-done
+find . -type d -empty
+find ~/ -type d -empty
 
-for folderList in $(find ~/ -maxdepth 1 -type d | sort); do
-    for emptyDir in $(find $folderList -type d -empty | sort); do
-        rm -rfv "$emptyDir"
-    done
-done
-
-if [[ $(command -v fsck | egrep /) != $null ]]; then
+if command -v fsck | grep -e /; then
     find /dev/ -type d | xargs -I% fsck -f -R -y %
 fi
 
-if [ $(cat /etc/modprobe.d/usbhid.conf | egrep "options usbhid mousepoll=1") != $null ]; then
+if ! cat /etc/modprobe.d/usbhid.conf | grep -e "options usbhid mousepoll=1"; then
     echo "options usbhid mousepoll=1" >>/etc/modprobe.d/usbhid.conf
     echo "options usbhid kbpoll=1" >>/etc/modprobe.d/usbhid.conf
     echo "options usbhid jspoll=1" >>/etc/modprobe.d/usbhid.conf
 fi
 
-cat /sys/module/usbhid/parameters/mousepoll
-RESULT=$?
-if [ $RESULT == 0 ]; then
+if cat /sys/module/usbhid/parameters/mousepoll; then
     echo 1 >/sys/module/usbhid/parameters/mousepoll
 fi
 
-cat /sys/module/usbhid/parameters/kbpoll
-RESULT=$?
-if [ $RESULT == 0 ]; then
+if cat /sys/module/usbhid/parameters/kbpoll; then
     echo 1 >/sys/module/usbhid/parameters/kbpoll
 fi
 
-cat /sys/module/usbhid/parameters/jspoll
-RESULT=$?
-if [ $RESULT == 0 ]; then
+if cat /sys/module/usbhid/parameters/jspoll; then
     echo 1 >/sys/module/usbhid/parameters/jspoll
 fi
 
-if [[ $(command -v apt | egrep /) != $null ]]; then
+if command -v apt; then
     add-apt-repository multiverse -y
     add-apt-repository ppa:obsproject/obs-studio -y
     add-apt-repository ppa:libretro/stable -y
@@ -80,76 +61,76 @@ if [[ $(command -v apt | egrep /) != $null ]]; then
     add-apt-repository ppa:graphics-drivers/ppa -y
     # Linux mint repo
     echo "deb http://packages.linuxmint.com una upstream" | sudo tee /etc/apt/sources.list.d/mint-una.list
-    sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com A1715D88E1DF1F24 40976EAF437D05B5 3B4FE6ACC0B21F32 A6616109451BBBF2
+    apt-key adv --recv-keys --keyserver keyserver.ubuntu.com A1715D88E1DF1F24 40976EAF437D05B5 3B4FE6ACC0B21F32 A6616109451BBBF2
+    # Microsoft
+    curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+    apt-add-repository https://packages.microsoft.com/debian/10/prod
+    apt-get update
     apt update && apt install aptitude snapd -y
 fi
 
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo apt-add-repository https://packages.microsoft.com/debian/10/prod
-sudo apt-get update
-
 ${shellConst} -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-if [[ $(command -v snap | egrep /) != $null ]]; then
+if command -v snap; then
     snap install ${corePkgs} -y
     snakeInstall "snap uninstall python2 python -y; snap install python3 -y"
     exit 0
-elif [[ $(command -v aptitude | egrep /) != $null ]]; then
+elif command -v aptitude; then
     aptitude update
     aptitude install ${corePkgs} -y
     snakeInstall "aptitude uninstall python2 python -y; aptitude install python3 -y"
     python -m pip install -U apt-mirror-updater && apt-mirror-updater -a
-    if [[ $(lspci | egrep VGA | egrep geforce) != $null ]]; then
+    if lspci | grep -e VGA | grep -e geforce; then
         aptitude install nvidia-driver-510 -y
     fi
     aptitude upgrade -y
     exit 0
-elif [[ $(command -v apt | egrep /) != $null ]]; then
+elif command -v apt; then
     apt update
     apt install ${corePkgs} -y
     snakeInstall "apt uninstall python2 python -y; apt install python3 -y"
     python -m pip install -U apt-mirror-updater && apt-mirror-updater -a
-    if [[ $(lspci | egrep VGA | egrep geforce) != $null ]]; then
+    if lspci | grep -e VGA | grep -e geforce; then
         apt install nvidia-driver-510 -y
     fi
     apt full-upgrade -y && apt autoremove -y && apt autoclean -y && apt --fix-broken install -y
     exit 0
-elif [[ $(command -v brew | egrep /) != $null ]]; then
+elif command -v brew; then
     brew install ${corePkgs} -y
     snakeInstall "brew uninstall python2 python -y; brew install python3 -y"
     exit 0
-elif [[ $(command -v yay | egrep /) != $null ]]; then
+elif command -v yay; then
     yay -S ${corePkgs} --noconfirm
     snakeInstall "yay -R python2 python --noconfirm; yay -S python3 --noconfirm"
-    if [[ $(lspci | egrep VGA | egrep geforce) != $null ]]; then
+    if lspci | grep -e VGA | grep -e geforce; then
         yay -S nvidia --noconfirm
     fi
     yay -Syuu
     exit 0
-elif [[ $(command -v pacman | egrep /) != $null ]]; then
+elif command -v pacman; then
     pacman -S ${corePkgs} --noconfirm
     snakeInstall "pacman -R python2 python --noconfirm; pacman -S python3 --noconfirm"
-    if [[ $(lspci | egrep VGA | egrep geforce) != $null ]]; then
+    if lspci | grep -e VGA | grep -e geforce; then
         pacman -S nvidia --noconfirm
     fi
     pacman -Syuu
     exit 0
-elif [[ $(command -v zypper | egrep /) != $null ]]; then
+elif command -v zypper; then
     zypper install ${corePkgs} -y
     snakeInstall "zypper rr python2 python -y; zypper install python3 -y"
     exit 0
-elif [[ $(command -v yum | egrep /) != $null ]]; then
+elif command -v yum; then
     yum install ${corePkgs} -y
     snakeInstall "yum remove python2 python -y; yum install python3 -y"
     exit 0
-elif [[ $(command -v dnf | egrep /) != $null ]]; then
+elif command -v dnf; then
     dnf install ${corePkgs} -y
     snakeInstall "zypper rr python2 python -y; dnf install python3 -y"
     exit 0
-elif [[ $(command -v port | egrep /) != $null ]]; then
+elif command -v port; then
     port upgrade ${corePkgs} -y
     snakeInstall "port uninstall python2 python -y; port upgrade install python3 -y"
     exit 0
+else
+    exit 1
 fi
-
-exit 0
--- startup/launch.bat
+++ startup/launch.bat
@@ -211,13 +211,15 @@ IF /I %M%==Y ( exit )
 
 powershell.exe -c choco upgrade chocolatey 7zip adb aria2 dos2unix ffmpeg firefox git jq mpv nomacs openvpn powershell rsync scrcpy smplayer unison vlc -y
 powershell.exe -c choco upgrade audacious audacity discord filezilla foobar2000 kodi libreoffice microsoft-edge obs-studio okular picard pinta qbittorrent steam vscode -y
+
 choco uninstall python2 python -y & choco upgrade python3 -y 
 aria2c -x16 -s32 -R --allow-overwrite=true https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 
-python -m pip install -U pip wheel beautysh notebook virtualenv ipykernel yt-dlp youtube-dl
+python -m pip install -U pip wheel beautysh notebook virtualenv ipykernel jupyter-themes yt-dlp youtube-dl
 :: python -m pip install -U git+https://github.com/samloader/samloader.git
 :: python -m pip install -U pymusiclooper spleeter
+jt -t monokai 
 
 cmd.exe /c "echo y|powershell.exe -c Install-Module PSWindowsUpdate -Force"  
 cmd.exe /c "echo y|powershell.exe -c Add-WUServiceManager -MicrosoftUpdate"
