a0dab1c9e8406c24d44fb5e85fdabbd890ab1439
Zerohazard8x <75481019+Zerohazard8x@users.noreply.github.com>
2023-08-08 23:37:53 +0800

    reposition

--- setup_linux_progs.sh
+++ setup_linux_progs.sh
@@ -3,9 +3,9 @@ sudo -i
 ariaPathConst=$(command -v aria2c | sort -r | head -n 1)
 shellConst=$(command -v "$SHELL" | sort -r | head -n 1)
 
-corePkgs="7zip adb aria2 dos2unix firefox ffmpeg git jq miktex mpv nano nomacs openvpn powershell phantomjs rsync scrcpy shfmt smplayer unison vlc"
+corePkgs="7zip adb aria2 dos2unix firefox ffmpeg git jq mpv nano nomacs openvpn powershell phantomjs rsync scrcpy shfmt smplayer unison vlc"
 # plusPkgs="audacity discord foobar2000 kodi libreoffice obsidian obs-studio pdfsam picard pinta qbittorrent steam vscode"
-# otherPkgs="audacious alacritty blender chromium czkawka darktable doomsday filezilla ioquake3 jdownloader kdenlive meld microsoft-edge neovim okular opera parsec pdfsam retroarch tor-browser vscodium wezterm"
+# otherPkgs="audacious alacritty blender chromium czkawka darktable doomsday filezilla ioquake3 jdownloader kdenlive meld microsoft-edge miktex neovim okular opera parsec pdfsam retroarch tor-browser vscodium wezterm"
 
 snakeInstall() {
     echo "$1" | ${shellConst}
